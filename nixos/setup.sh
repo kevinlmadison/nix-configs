6f23d8ea984e0b28a4342fedee098881a4049813
kevinlmadison <coolklm121@gmail.com>
2024-02-09 18:13:25 -0500

    slighly better setup script, doesn't rely on input

--- nixos/setup.sh
+++ nixos/setup.sh
@@ -3,13 +3,32 @@
 # Usage: ./setup.sh <hostname>
 
 setup() {
-  user=$(whoami)
-
+  local user=$(whoami)
+  local host=$(hostname)
 
   # Move raw configs from platform to .config dir
-  for dir in $(ls ./hosts/"$1"/configs);
+  for dir in $(ls ./hosts/"$host"/configs);
   do
-    ln -s ./hosts/"$1"/configs/"$dir" /home/"$user"/.config/"$dir";
+
+    dir_link=/home/"$user"/.config/"$dir"
+    dir_path=./hosts/"$host"/configs/"$dir"
+
+    if [[ ! -d "$dir_link" ]]; then
+
+      ln -s "$dir_path" "$dir_link"
+
+    elif [[ -L "$dir_link" ]]; then
+
+      rm -rf "$dir_link"
+      ln -s "$dir_path" "$dir_link"
+
+    else
+
+      mv "$dir_link" "$dir_link".bak
+      ln -s "$dir_path" "$dir_link"
+
+    fi
+
   done
 }
 
