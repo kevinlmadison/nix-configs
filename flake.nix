1a378a921a74ea94d81f2e611e72ac6bb8430c95
Kevin Madison <kelevra@Kevins-MacBook-Pro.local>
2024-02-11 14:56:06 -0500

    Got initial setup on MacOS working, nothing installed yet though.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,26 @@
   inputs = {
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nix-darwin, ... }@inputs: {
+    darwinConfigurations = {
+      "Kevins-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+	  ./hosts/m3/default.nix 
+        ];
+      };
+    };
+    darwinPackages = self.darwinConfigurations."Kevins-MacBook-Pro".pkgs;
+
     nixosConfigurations = {
 
       "thinkpad" = nixpkgs.lib.nixosSystem {
