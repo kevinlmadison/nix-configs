d557ef5edf918bfa0e88280f67927760ee9a9541
kevinlmadison <coolklm121@gmail.com>
2024-02-10 22:41:57 -0500

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
-# Usage: ./setup.sh <hostname>
-
-setup() {
-  local user=$(whoami)
+setup_config_dir() {
+  local pwd=$(pwd)
   local host=$(hostname)
 
   # Move raw configs from platform to .config dir
-  for dir in $(ls ./hosts/"$host"/configs);
+  for dir in $(ls "$pwd"/hosts/"$host"/configs);
   do
 
-    dir_link=/home/"$user"/.config/"$dir"
-    dir_path=./hosts/"$host"/configs/"$dir"
+    dir_link=~/.config/"$dir"
+    dir_path="$pwd"/hosts/"$host"/configs/"$dir"
 
-    if [[ ! -d "$dir_link" ]]; then
-
-      ln -s "$dir_path" "$dir_link"
-
-    elif [[ -L "$dir_link" ]]; then
-
-      rm -rf "$dir_link"
-      ln -s "$dir_path" "$dir_link"
-
-    else
+    # If we're in a fresh state, create a backup
+    if [[   -d "$dir_link" &&
+          ! -L "$dir_link" &&
+          ! -d "$dir_link".bak
+    ]]; then
 
       mv "$dir_link" "$dir_link".bak
-      ln -s "$dir_path" "$dir_link"
+
+    # If a symlink exists already, remove it
+    elif [[ -d "$dir_link" &&
+            -L "$dir_link"
+    ]]; then
+
+      rm -rf "$dir_link"
 
     fi
+
+    # Create a symlink to the config dir in git
+    ln -s "$dir_path" "$dir_link"
 
   done
 }
 
-setup
+setup_config_dir
