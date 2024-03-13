9d80d5bb42ef5c8ea7381941e3b180d4762821b1
kevinlmadison <coolklm121@gmail.com>
2024-03-13 14:01:24 -0400

    O

--- hosts/common/system.nix
+++ hosts/common/system.nix
@@ -77,6 +77,23 @@
     ##  thunderbird
     #];
   };
+  nix.buildMachines = [ {
+	  hostName = "builder";
+	  system = "x86_64-linux";
+    protocol = "ssh-ng";
+	  # if the builder supports building for multiple architectures, 
+	  # replace the previous line by, e.g.
+	  # systems = ["x86_64-linux" "aarch64-linux"];
+	  maxJobs = 1;
+	  speedFactor = 2;
+	  supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+	  mandatoryFeatures = [ ];
+	}] ;
+	nix.distributedBuilds = true;
+	# optional, useful when the builder has a faster internet connection than yours
+	nix.extraOptions = ''
+	  builders-use-substitutes = true
+	'';
   nix.settings = {
     experimental-features = [ "flakes" "nix-command" ];
     # given the users in this list the right to specify additional substituters via:
