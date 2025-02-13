a6a6114e8acf0553b4ee65e0498e84fe55e797b5
kevinlmadison <coolklm121@gmail.com>
2025-02-13 17:12:13 -0500

    fix updated hyprland config

--- hosts/xps/default.nix
+++ hosts/xps/default.nix
@@ -12,9 +12,10 @@
   ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/nvme0n1";
+  # boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   networking.hostName = "xps"; # Define your hostname.
